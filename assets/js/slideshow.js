7945ac3263e66f3323e877b51ef38b167b2d4e7d
Aaron <aaron.jee.ho@gmail.com>
2020-10-22 05:21:53 -0700

    Added Indicators for current image in slideshow

--- assets/js/slideshow.js
+++ assets/js/slideshow.js
@@ -8,9 +8,11 @@ play_slides();
 function play_slides() {
   var i;
   var slides = document.getElementsByClassName("slide_img");
+  var dots = document.getElementsByClassName("dot");
 
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none"; 
+    slides[i].style.display = "none";
+    dots[i].style.background = "#888"
   }
 
   curr_index++;
@@ -20,5 +22,6 @@ function play_slides() {
   }
 
   slides[curr_index - 1].style.display = "block";
+  dots[curr_index - 1].style.background = "#286086"
   setTimeout(play_slides, 5000)
 }
